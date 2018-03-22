b7ac1f1558fd58aa9453d78203ce29aaedc918e5
danigracia <gracia.danii@gmail.com>
2018-03-22 12:23:48 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace ExampleEnvironment
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Environment.CurrentDirectory = Environment.GetEnvironmentVariable("TEST");
+            DirectoryInfo info = new DirectoryInfo(".");
+
+            Console.WriteLine("Directory Info:   " + info.FullName);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
